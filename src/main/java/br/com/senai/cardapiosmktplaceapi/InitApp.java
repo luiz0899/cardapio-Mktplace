66ef09ff2b8527e5d7b23543f5e8e3cd323519c6
LUIZ HENRIQUE PEREIRA CORREA <luiz_h_correa@estudante.sc.senai.br>
2023-08-30 21:41:41 -0300

    first commit

---
+++ src/main/java/br/com/senai/cardapiosmktplaceapi/InitApp.java
@@ -0,0 +1,27 @@
+package br.com.senai.cardapiosmktplaceapi;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+public class InitApp {
+	
+	public static void main(String[] args) {
+	
+		SpringApplication.run(InitApp.class,args);
+			
+	}
+	@Bean
+	public CommandLineRunner commandLineRunner(ApplicationContext ctx ) {
+		
+		return args -> {
+
+			System.out.println("subiu"); 
+		
+		};
+	}
+	
+}
---
+++ src/main/java/br/com/senai/cardapiosmktplaceapi/entity/enums/Status.java
@@ -0,0 +1,8 @@
+package br.com.senai.cardapiosmktplaceapi.entity.enums;
+
+public enum Status {
+	
+	A,
+	I
+
+}
